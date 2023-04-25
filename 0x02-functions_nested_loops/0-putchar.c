eb3d3b9fd121681647503736c3ea6ac5cca35bc9
TiffanyChy <105989065+TiffanyChy@users.noreply.github.com>
2023-04-25 11:39:09 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - print holberton
+ * main - print _putchar
  * Return: 0 if succesful
  */
 int main(void)
 {
-:
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);
